d720291d8b50017af9ae162f537100a9b4063b15
KateOblomova <kate.oblom@gmail.com>
2023-09-13 16:08:02 +0200

    added fetching in assets and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import RecipesList from "./assets/Fetching";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [recipes, setRecepies] = useState([]); //fetched recipes are in this state, pass them down to your component as props
+  const { getRecipes } = RecipesList();
+
+  useEffect(() => {
+    getRecipes().then((data) => setRecepies(data.items));
+  }, []);
+
+  console.log(recipes);
+
+  return (
+    <>
+      <div>
+        {/* <Routes>
+          <Route path="/desert/:hello" element={} /> //Here to pass component of recipe with props to navigate thru each recipe with help of useParams()
+          <Route path="/starter/:hello" element={} />
+          <Route path="/main/:hello" element={} />
+          <Route path="/drink/:hello" element={} />
+        </Routes> */}
+      </div>
+    </>
+  );
 }
 
 export default App;
