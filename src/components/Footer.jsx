aaeea93a30a4eac02edc22fafde153b51af6879f
nitamathur <nitamathur33@gmail.com>
2023-09-18 11:43:14 +0200

    my first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,48 @@ import { NavLink } from "react-router-dom";
 export default function Footer() {
     return (
       <>
-      <footer style={{display: "grid", borderTopStyle: "solid", borderTopColor: "lightgrey", fontSize:"25px"}}>
-        <hr/>
-      <NavLink to="">Footer #1 </NavLink>
+       <div class="footer" id="footer">
+                    <div class="footer-content">
+                        <div class="stay-touch">
+                         <h2>Stay in Touch</h2>
+                        <p> Please subscribe to our newsletter to get the latest news in your domain of interest. <br>
+                            </br>Don't forget to follow us on social networks!</p>
+                <div class="s-icon">
+
+                  <i class="fa-brands fa-facebook"></i>
+                  <i class="fa-brands fa-twitter"></i>
+                  <i class="fa-brands fa-instagram"></i>
+                  <i class="fa-brands fa-pinterest"></i>
+
+                   </div>
+                </div>
+                      
+                   <div class="email-bar">
+                    <div class="input-email">
+                       <input class="input-button" name="email" id="" placeholder="Email Address" required/> 
+                    </div>
+                    <div class="email-button">
+                        <button class="submit-button" type="submit" name="subscribe" >SUBSCRIBE</button>
+                    </div>
+            
+                   </div>
+                         </div>
+          
+            
+            
+            <p class="copyright" id="copyright1">Copyright © 2023  All Rights Reserved <br> 
+                  </br> Proudly hosted by Group-1</p>
+                
+        
+        
+            
+      
+
+
+      {/* <NavLink to="">Footer #1 </NavLink>
       <NavLink to="">Footer #2 </NavLink>
-      <NavLink to="">Footer #3 </NavLink>
-      </footer>
+      <NavLink to="">Footer #3 </NavLink> */}
+      </div>
       </>  
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,38 @@ export default function Navbar() {
 
     return (
       <>
-      <header style={{display: "flex", justifyContent: "space-evenly", alignItems: "center", fontSize:"25px", textDecoration: "none"}}>
-      <NavLink to="/"> <img src="https://media.istockphoto.com/id/1307257578/vector/cook-book-line-icon.jpg?s=612x612&w=0&k=20&c=tqwS3hHnUsRc_vJPDV-PLMoAZ_WmQj0c-6bzfK9Xpyw=" alt="logo" width="75px" /> </NavLink>
-      <NavLink to="starter">Starters </NavLink>
-      <NavLink to="main">Main Dishes </NavLink>
-      <NavLink to="dessert">Desserts </NavLink>
-      <NavLink to="drink">Drinks</NavLink>
-      <NavLink to="allrecipes">All Recipes</NavLink>
-      </header>
-      <hr/>
+      <div className="header-container">
+        <ul>
+          <li>
+          <NavLink to="/"> <img src="https://media.istockphoto.com/id/1307257578/vector/cook-book-line-icon.jpg?s=612x612&w=0&k=20&c=tqwS3hHnUsRc_vJPDV-PLMoAZ_WmQj0c-6bzfK9Xpyw=" alt="logo" width="75px" /> </NavLink>
+          </li>
+          <li>
+         <NavLink to="starter">Starters </NavLink>
+         </li>
+       <li>
+       <NavLink to="main">Main Dishes </NavLink>
+        </li>  
+        <li>
+          <NavLink to="dessert">Desserts </NavLink>
+          </li>
+          <li>
+          <NavLink to="drink">Drinks</NavLink>
+          </li>
+          <li>
+          <NavLink to="allrecipes">All Recipes</NavLink>
+          </li>
+        </ul>
+        </div>
+        
+
+
+        
+
+        
+      
+     
+      
+      
       </>  
     )
 }
