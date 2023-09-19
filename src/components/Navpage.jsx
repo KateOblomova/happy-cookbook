c2b412bb3a5cef9c793ffc5b2f756b111a98c2fb
KateOblomova <kate.oblom@gmail.com>
2023-09-19 15:24:09 +0200

    fixed images and token

--- src/components/Navpage.jsx
+++ src/components/Navpage.jsx
@@ -9,70 +9,101 @@ export default function Navpage({ recipes, searchValue }) {
       setIsLoading(false);
     }, 1500);
     return () => clearTimeout(timer);
-  },[])
+  }, []);
   // Search array
-  const filteredRecipes = recipes.filter((item) => 
-  item.fields.recipeName.toLowerCase().includes(searchValue.toLowerCase())
+  const filteredRecipes = recipes.filter((item) =>
+    item.fields.recipeName.toLowerCase().includes(searchValue.toLowerCase())
   );
 
-  // Getting path name and "cleaning" it so that it matches the category 
+  // Getting path name and "cleaning" it so that it matches the category
   const capitaliseFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-  
+  };
+
   let path = window.location.pathname;
-  path = path.replace("/","");
+  path = path.replace("/", "");
   path = capitaliseFirstLetter(path);
 
-// this comment doesn't say anything
-// Function to sort arrays from A-Z
-const alphabeticiseArray = (arr) => {
-  return arr.sort((a,b) => {
-    const nameA = a.fields.recipeName.toUpperCase();
-    const nameB = b.fields.recipeName.toUpperCase();
-    if (nameA < nameB) {
-      return -1;
-    }
-    if (nameA > nameB) {
-      return 1
-    }
-    return 0;
-  });
-}
-alphabeticiseArray(recipes);
+  // this comment doesn't say anything
+  // Function to sort arrays from A-Z
+  const alphabeticiseArray = (arr) => {
+    return arr.sort((a, b) => {
+      const nameA = a.fields.recipeName.toUpperCase();
+      const nameB = b.fields.recipeName.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+  };
+  alphabeticiseArray(recipes);
 
-// ready to use API data
-console.log(recipes);
-console.log(path);
-const loadingArray = ["One moment please", "Turn that oven on", "Sharpen that knife", "Wash your hands", "Hope you are hungry", "Prepare for greatness"];
-return (
-        <>
-        <h1 style={{textAlign: "center"}}>{path.replace("/","")} Page</h1>
-        {isLoading ? (<div style={{display: "flex", justifyContent: "center", alignItems:"center", flexDirection: "column"}}><SpinnerDotted
-        loading ={isLoading}
-        data-testid="loader"
-        color={"orange"}
-        size={300}
-        />
-        <h2 style={{color: "grey"}}>{loadingArray[Math.floor(Math.random() * 6)]}</h2>
+  // ready to use API data
+  console.log(recipes);
+  console.log(path);
+  const loadingArray = [
+    "One moment please",
+    "Turn that oven on",
+    "Sharpen that knife",
+    "Wash your hands",
+    "Hope you are hungry",
+    "Prepare for greatness",
+  ];
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>{path.replace("/", "")} Page</h1>
+      {isLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <SpinnerDotted
+            loading={isLoading}
+            data-testid="loader"
+            color={"orange"}
+            size={300}
+          />
+          <h2 style={{ color: "grey" }}>
+            {loadingArray[Math.floor(Math.random() * 6)]}
+          </h2>
         </div>
-        ) : (
-      <div className="navPage-Container" style={{display: "flex", justifyContent: "space-evenly", flexWrap:"wrap"}}>      
-      {filteredRecipes.map((text, index) => {
-        if (text.fields.category === path || path === "Allrecipes") {
-          return (
-            <div key={index}>  
-            <Link to={`/${text.fields.category.toLowerCase()}/${text.sys.id}`}>
-            <img style={{borderRadius: "30px"}} src={text.fields.picture.fields.file.url} width="350px" height="350px"/>
-            </Link>
-            <h2>{text.fields.recipeName}</h2> 
-            </div>
-          );
-        }
-        return null;
-      })}
-      </div>
+      ) : (
+        <div
+          className="navPage-Container"
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            flexWrap: "wrap",
+          }}
+        >
+          {filteredRecipes.map((text, index) => {
+            if (text.fields.category === path || path === "Allrecipes") {
+              return (
+                <div key={index} className="categoryMealsCards">
+                  <Link
+                    to={`/${text.fields.category.toLowerCase()}/${text.sys.id}`}
+                  >
+                    <img
+                      className="categoryMealsPicture"
+                      style={{ borderRadius: "30px" }}
+                      src={text.fields.picture.fields.file.url}
+                    />
+                  </Link>
+                  <h2>{text.fields.recipeName}</h2>
+                </div>
+              );
+            }
+            return null;
+          })}
+        </div>
       )}
-      </>
-    )
+    </>
+  );
 }
