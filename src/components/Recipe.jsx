053357576fb6b1195159b1b0ab4c827f70674e8c
DarrellRoberts <mrdarrellroberts@gmail.com>
2023-09-27 20:17:29 +0200

    slight tweak to dark mode

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -73,7 +73,7 @@ export default function Recipe() {
           </ol>
         </div>
       </div>
-      <div id="buttons">
+      <div style={{backgroundColor: themeStyles.background}} id="buttons">
         <button
           onClick={() =>
             navigate(
